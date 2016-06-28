369fe83ff44892fbf647a65ee281e3050a527ed6
wangpeng <wangpeng@dsdl.com>
2016-06-28 17:36:18 +0800

    新建了项目

---
+++ MyGit/MyGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGit
+//
+//  Created by 王鹏 on 16/6/28.
+//  Copyright © 2016年 鼎商动力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyGit/MyGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyGit
+//
+//  Created by 王鹏 on 16/6/28.
+//  Copyright © 2016年 鼎商动力. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
